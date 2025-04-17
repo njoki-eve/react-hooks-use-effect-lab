475e4e47c51bbb6c20312f324b4a0d96473ef296
njoki-eve <njokieverlyne71@gmail.com>
2025-04-17 09:21:02 +0300

    codegrade

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Set up the timer
+    const timerId = setTimeout(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime <= 1) {
+          // Time is up - reset timer and notify parent
+          onAnswered(false);
+          return 10;
+        } else {
+          // Decrement time by 1
+          return prevTime - 1;
+        }
+      });
+    }, 1000);
+
+    // Cleanup function to clear the timeout
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]); // Dependencies array
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
